6e5bfda9ef274d0d5138b71360ac24b8788b5933
bobisi <dimulya.zaytsev.2014@mail.ru>
2025-06-02 19:13:49 +0300

    Add files via upload

---
+++ garry-master/settings.py
@@ -0,0 +1,22 @@
+# settings.py
+
+# Размер окна
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 900
+
+# Размер тайла
+TILE_SIZE = 50
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+SKY_BLUE = (135, 206, 235)
+
+# Игровые параметры
+GRAVITY = 1
+JUMP_STRENGTH = 20
+MOVE_SPEED = 8
+MAX_FALL_SPEED = 25
+
+# FPS
+FPS = 60
